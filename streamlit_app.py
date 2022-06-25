bef7698959487d7e84ee97e50bc7bc77cbe52140
gaimps <gaimps@hotmail.com>
2022-06-25 15:21:14 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title(🍌🥭'My Parent new healthy Diner'🥝🍇)
+streamlit.title('🍌🥭My Parent new healthy Diner🥝🍇')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text(🥣'Omega 3 & blueberry Oatmeal')
-streamlit.text(🥗'Kale,Spinach & Rocekt Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocekt Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
