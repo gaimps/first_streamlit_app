f48c45c4e37e4c113179401bc1de2c8a9fa56aa5
gaimps <gaimps@hotmail.com>
2022-06-25 15:14:13 -0400

    Update streamlit_app.py
    addin new text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parent new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocekt Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
