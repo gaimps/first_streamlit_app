df08acd4fe1f9310c9595ebeea1e1a7686adee88
gaimps <gaimps@hotmail.com>
2022-06-25 15:42:18 -0400

    adding a picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,12 @@ streamlit.text('🥗Kale,Spinach & Rocekt Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruits_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+# pick list
+streamlit.multiselect("Pick some fruits", list(my_fruits_list.index))
+
+# display the table
 streamlit.dataframe(my_fruits_list)
