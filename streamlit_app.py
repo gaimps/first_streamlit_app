bc5266596f955fd0e2906267d85de0e49101d880
gaimps <gaimps@hotmail.com>
2022-07-06 23:20:08 -0400

    adding new box

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,12 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit = streamlit.text_input(
+    "what fruit would you like to add? ", "jackfruit")
+'''my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("insert into fruit_load_list")
+my_data_rows = my_cur.fetchall()'''
+streamlit.text("Thanks for adding" + add_my_fruit)
